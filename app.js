9140ecdfcbe5535e19cafe18edf4eeca8f5bae32
John McLear <john@mclear.co.uk>
2015-02-17 01:35:01 +0000

    get apool atest and also show new changes as they land

--- app.js
+++ app.js
@@ -15,12 +15,26 @@ socket.on('message', function(obj){
  
   // Client is connected so we should start sending messages at the server
   if(type === 'CLIENT_VARS'){
+    padState.atext = obj.data.collab_client_vars.initialAttributedText;
+    padState.apool = obj.data.collab_client_vars.apool;
     beginSendingMessages(obj);
   }
 
-  // Get the new Revision number
+  // Get the new Revision number from a change
   else if(obj.type === 'COLLABROOM' && obj.data && obj.data.type === 'NEW_CHANGES'){
+    console.log("new changes", obj.data);
+    var unpacked = Changeset.unpack(obj.data.changeset); // Unpack the changeset
+    var opiterator = Changeset.opIterator(unpacked.ops); // Look at each op
+    console.log("opiterator", opiterator);
     padState.newRev = obj.data.newRev;
+  }
+
+  else if(obj.type === 'COLLABROOM' && obj.data && obj.data.type === 'USER_NEWINFO'){
+    // We don't care about this.
+  }
+
+  else if(obj.type === 'COLLABROOM' && obj.data && obj.data.type === 'USER_LEAVE'){
+    // We don't care about this.
   }
 
   else{ // Unhandled message
@@ -30,20 +44,22 @@ socket.on('message', function(obj){
 
 function beginSendingMessages(obj){
   console.log("Beggining to send messages to the Pad");
+  var opiterator = Changeset.opIterator(padState.atext.attribs); // This seems pointless
+
+  console.log("initial atext", padState.atext);
+  console.log("initial apool", padState.apool);
 
   // Send a message every second
   setInterval(function(){
-
+    /*
     var msg = {
       "component": "pad",
       "type": 'USER_CHANGES',
       "baseRev": padState.newRev, // TODO
       "changeset": "Z:6l>1|8=6k*0+1$x", // TODO,
       "apool": { // TODO
-        nextNum: 1,
-        numToAttrib: {
-          0: ["author", "a.XHPfn6F9btKuwP0Y"]
-        }
+        nextNum: padState.apool.length+1,
+        numToAttrib: padState.apool
       }
     };
  
@@ -52,13 +68,19 @@ function beginSendingMessages(obj){
       component: "pad",
       data: msg
     });
+
+    */
+
   }, 1000);
+
 }
 
 socket.on('connect', function(data){
   var padId = 'test';
   sessionID = 'whatever';
   token = 'test';
+
+  console.log("Sending Client Ready");
 
   var msg = {
     "component": "pad",
