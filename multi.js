ac42a69396b89c17a1855f4f38f7295acfefb12b
John McLear <john@mclear.co.uk>
2020-11-06 21:46:04 +0000

    still not working properly

--- multi.js
+++ multi.js
@@ -1,22 +1,14 @@
-// parent.js
+// tests 10 pads (unless specified by "node multi.js X[int]" for 30 seconds
 var child_process = require('child_process');
 
-var maxPads = 10;
+var maxPads = process.argv[2] || 10; // 10 padIds
 var padCount = 0;
 var messageCount = 0;
 
 while(padCount < maxPads){
-  var child = child_process.fork('./app', ['-a', 3]);
+  var child = child_process.fork('./app', ['-a', 3, '-d', 30]);
   padCount++;
-  child.on('message', function(message) {
-    console.log('[parent] received message from child:', message);
-    if (numchild > maxPads) {
-      console.log("total pads made");
-      console.log("total messages", messageCount);
-      console.log('[parent] received all results');
-      process.exit(0);
-    }
-  });
+  // below still not working...
   child.on('error', function(){
     console.log("total pads made");
     console.log("total messages", messageCount);
