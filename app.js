f36e40367e908503f42d0fabbcd3419786bc986b
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2024-02-17 20:08:38 +0100

    Fixed linter

--- app.js
+++ app.js
@@ -14,193 +14,14 @@ const users = [];
 let loadUntilFail = false;
 const globalStats = {};
 let maxPS = 0;
-
-const randomPadName = () => { // From index.html
-  const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-  const strLen = 10;
-  let randomstring = '';
-  for (let i = 0; i < strLen; i++) {
-    const rnum = Math.floor(Math.random() * chars.length);
-    randomstring += chars.substring(rnum, rnum + 1);
-  }
-  return randomstring;
-};
-
-// Take Params and process them
-const args = argv.option(argvopts).run();
 let host;
-// Check for a host..
-if (process.argv[2] && process.argv[2].indexOf('http') !== -1) {
-  // It the arv2 item contains a hostname..
-  host = process.argv[2];
-  if (host.indexOf('/p/') === -1) { // No pad ID included so include one
-    host = `${host}/p/${randomPadName()}`;
-  }
-} else {
-  host = `http://127.0.0.1:9001/p/${randomPadName()}`;
-}
 
-if (args.options.lurkers) {
-  let i = 0;
-  while (i < args.options.lurkers) {
-    users.push('l');
-    i++;
-  }
-}
-if (args.options.authors) {
-  let x = 0;
-  while (x < args.options.authors) {
-    users.push('a');
-    x++;
-  }
-}
-let endTime;
-if (args.options.duration) {
-  endTime = startTimestamp + (args.options.duration * 1000);
-}
-
-// Check every second to see if currentTime is => endTime
-setInterval(() => {
-  const currentTime = Date.now();
-  if (currentTime > endTime) {
-    console.log('Test duration complete and Load Tests PASS');
-    console.log(stats.toJSON());
-    if (Object.keys(stats.toJSON()).length === 0) {
-      console.error("no test data gathered, perhaps loadTest wasn't enabled?");
-      process.exit(1); /* eslint-disable-line no-process-exit */
-    }
-    process.exit(0); /* eslint-disable-line no-process-exit */
-  }
-}, 100);
-
-// Create load until failure.
-const loadUntilFailFn = () => {
-  loadUntilFail = true;
-  // Loads at ratio of 3(lurkers):1(author), every 5 seconds it adds more.
-  const users = ['a', 'l', 'l', 'l'];
-
-  setInterval(() => {
-    async.eachSeries(users, (type, callback) => {
-      setTimeout(() => {
-        if (type === 'l') {
-          newLurker();
-          callback();
-        }
-        if (type === 'a') {
-          newAuthor();
-          callback();
-        }
-      }, 200 / (users.length || 1));
-    }, (err) => {
-    });
-  }, 1000); // every 5 seconds
-};
-
-
-// If there are authors / lurkers specified let's connect them up!
-if (args.options.authors || args.options.lurkers) {
-  async.eachSeries(users, (type, callback) => {
-    setTimeout(() => {
-      if (type === 'l') {
-        newLurker();
-        callback();
-      }
-      if (type === 'a') {
-        newAuthor();
-        callback();
-      }
-
-      //  }, 1000/(args.options.authors || 1));
-    }, 200 / (users.length || 1));
-    // All authors connect within 1 second but send messages on
-    // slightly different intervals
-    // This need slightly different logic
-  }, (err) => {
-  });
-} else {
-  if (!endTime) {
-    console.log('Creating load until the pad server stops responding in a timely fashion');
-  }
-  if (endTime) {
-    const testD = Math.round((endTime - Date.now()) / 1000);
-    console.log(`Creating load for ${testD} seconds`);
-  }
-  loadUntilFailFn();
-}
-
-// Creates a new author
-const newAuthor = () => {
-  const pad = etherpad.connect(host);
-  pad.on('socket_timeout', () => {
-    console.error('socket timeout connecting to pad');
-    process.exit(1); /* eslint-disable-line no-process-exit */
-  });
-  pad.on('socket_error', () => {
-    console.error('connection error connecting to pad, did you remember to set loadTest to true?');
-    process.exit(1); /* eslint-disable-line no-process-exit */
-  });
-  pad.on('connected', (padState) => {
-    globalStats.numConnectedUsers = padState.numConnectedUsers;
-    stats.meter('clientsConnected').mark();
-    stats.meter('authorsConnected').mark();
-    activeConnections.inc();
-    updateMetricsUI();
-
-    // console.log("Connected Author to", padState.host);
-    // Every second we send 4 characters
-    // Mean = 40 WPM = 240 characters/minute
-    // https://imlocation.wordpress.com/2007/12/05/how-fast-do-people-type/
-    // This simulates the Mean of an author
-    setInterval(() => {
-      stats.meter('appendSent').mark();
-      updateMetricsUI();
-      try {
-        pad.append(randomString()); // Appends 4 Chars
-      } catch (e) {
-        stats.meter('error').mark();
-        console.log('Error!');
-      }
-    }, 400);
-  });
-  pad.on('message', (msg) => {
-    if (msg.type !== 'COLLABROOM') return;
-    if (msg.data.type === 'ACCEPT_COMMIT') {
-      stats.meter('acceptedCommit').mark();
-    }
-  });
-  pad.on('newContents', (atext) => {
-    stats.meter('changeFromServer').mark();
-  });
-};
-
-// Creates a new lurker.
-const newLurker = () => {
-  const pad = etherpad.connect(host);
-  pad.on('socket_timeout', () => {
-    console.error('socket timeout connecting to pad');
-    process.exit(1); /* eslint-disable-line no-process-exit */
-  });
-  pad.on('socket_error', () => {
-    console.error('connection error connecting to pad, did you remember to set loadTest to true?');
-    process.exit(1); /* eslint-disable-line no-process-exit */
-  });
-  pad.on('connected', (padState) => {
-    globalStats.numConnectedUsers = padState.numConnectedUsers;
-    stats.meter('clientsConnected').mark();
-    stats.meter('lurkersConnected').mark();
-    updateMetricsUI();
-    // console.log("Connected new lurker to", padState.host);
-  });
-  pad.on('newContents', (atext) => {
-    stats.meter('changeFromServer').mark();
-  });
-};
 
 const randomString = () => {
   let randomstring = '';
   // var string_length = Math.floor(Math.random() *2);
-  const string_length = 4; // See above for WPM stats
-  for (let i = 0; i < string_length; i++) {
+  const stringLength = 4; // See above for WPM stats
+  for (let i = 0; i < stringLength; i++) {
     const charNumber = Math.random() * (300 - 1) + 1;
     const str = String.fromCharCode(parseInt(charNumber));
     // This method generates sufficient noise
@@ -256,8 +77,189 @@ const updateMetricsUI = () => {
     const diff = jstats.appendSent.count - jstats.acceptedCommit.count;
     if (diff > 5) {
       console.log('Number of commits not yet replied as ACCEPT_COMMIT from server', diff);
-      if (loadUntilFail && diff > 100) process.exit(1); /* eslint-disable-line no-process-exit */
+      if (loadUntilFail && diff > 100) process.exit(1); /* eslint-disable-line n/no-process-exit */
     }
   }
   console.log('Seconds test has been running for:', parseInt(testDuration / 1000));
 };
+
+// Creates a new author
+const newAuthor = () => {
+  const pad = etherpad.connect(host);
+  pad.on('socket_timeout', () => {
+    console.error('socket timeout connecting to pad');
+    process.exit(1); /* eslint-disable-line n/no-process-exit */
+  });
+  pad.on('socket_error', () => {
+    console.error('connection error connecting to pad, did you remember to set loadTest to true?');
+    process.exit(1); /* eslint-disable-line n/no-process-exit */
+  });
+  pad.on('connected', (padState) => {
+    globalStats.numConnectedUsers = padState.numConnectedUsers;
+    stats.meter('clientsConnected').mark();
+    stats.meter('authorsConnected').mark();
+    activeConnections.inc();
+    updateMetricsUI();
+
+    // console.log("Connected Author to", padState.host);
+    // Every second we send 4 characters
+    // Mean = 40 WPM = 240 characters/minute
+    // https://imlocation.wordpress.com/2007/12/05/how-fast-do-people-type/
+    // This simulates the Mean of an author
+    setInterval(() => {
+      stats.meter('appendSent').mark();
+      updateMetricsUI();
+      try {
+        pad.append(randomString()); // Appends 4 Chars
+      } catch (e) {
+        stats.meter('error').mark();
+        console.log('Error!');
+      }
+    }, 400);
+  });
+  pad.on('message', (msg) => {
+    if (msg.type !== 'COLLABROOM') return;
+    if (msg.data.type === 'ACCEPT_COMMIT') {
+      stats.meter('acceptedCommit').mark();
+    }
+  });
+  pad.on('newContents', (atext) => {
+    stats.meter('changeFromServer').mark();
+  });
+};
+
+// Creates a new lurker.
+const newLurker = () => {
+  const pad = etherpad.connect(host);
+  pad.on('socket_timeout', () => {
+    console.error('socket timeout connecting to pad');
+    process.exit(1); /* eslint-disable-line n/no-process-exit */
+  });
+  pad.on('socket_error', () => {
+    console.error('connection error connecting to pad, did you remember to set loadTest to true?');
+    process.exit(1); /* eslint-disable-line n/no-process-exit */
+  });
+  pad.on('connected', (padState) => {
+    globalStats.numConnectedUsers = padState.numConnectedUsers;
+    stats.meter('clientsConnected').mark();
+    stats.meter('lurkersConnected').mark();
+    updateMetricsUI();
+    // console.log("Connected new lurker to", padState.host);
+  });
+  pad.on('newContents', (atext) => {
+    stats.meter('changeFromServer').mark();
+  });
+};
+
+
+const randomPadName = () => { // From index.html
+  const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  const strLen = 10;
+  let randomstring = '';
+  for (let i = 0; i < strLen; i++) {
+    const rnum = Math.floor(Math.random() * chars.length);
+    randomstring += chars.substring(rnum, rnum + 1);
+  }
+  return randomstring;
+};
+
+// Take Params and process them
+const args = argv.option(argvopts).run();
+// Check for a host..
+if (process.argv[2] && process.argv[2].indexOf('http') !== -1) {
+  // It the arv2 item contains a hostname..
+  host = process.argv[2];
+  if (host.indexOf('/p/') === -1) { // No pad ID included so include one
+    host = `${host}/p/${randomPadName()}`;
+  }
+} else {
+  host = `http://127.0.0.1:9001/p/${randomPadName()}`;
+}
+
+if (args.options.lurkers) {
+  let i = 0;
+  while (i < args.options.lurkers) {
+    users.push('l');
+    i++;
+  }
+}
+if (args.options.authors) {
+  let x = 0;
+  while (x < args.options.authors) {
+    users.push('a');
+    x++;
+  }
+}
+let endTime;
+if (args.options.duration) {
+  endTime = startTimestamp + (args.options.duration * 1000);
+}
+
+// Check every second to see if currentTime is => endTime
+setInterval(() => {
+  const currentTime = Date.now();
+  if (currentTime > endTime) {
+    console.log('Test duration complete and Load Tests PASS');
+    console.log(stats.toJSON());
+    if (Object.keys(stats.toJSON()).length === 0) {
+      console.error("no test data gathered, perhaps loadTest wasn't enabled?");
+      process.exit(1); /* eslint-disable-line n/no-process-exit */
+    }
+    process.exit(0); /* eslint-disable-line n/no-process-exit */
+  }
+}, 100);
+
+// Create load until failure.
+const loadUntilFailFn = () => {
+  loadUntilFail = true;
+  // Loads at ratio of 3(lurkers):1(author), every 5 seconds it adds more.
+  const users = ['a', 'l', 'l', 'l'];
+
+  setInterval(() => {
+    async.eachSeries(users, (type, callback) => {
+      setTimeout(() => {
+        if (type === 'l') {
+          newLurker();
+          callback();
+        }
+        if (type === 'a') {
+          newAuthor();
+          callback();
+        }
+      }, 200 / (users.length || 1));
+    }, (_) => {
+    });
+  }, 1000); // every 5 seconds
+};
+
+
+// If there are authors / lurkers specified let's connect them up!
+if (args.options.authors || args.options.lurkers) {
+  async.eachSeries(users, (type, callback) => {
+    setTimeout(() => {
+      if (type === 'l') {
+        newLurker();
+        callback();
+      }
+      if (type === 'a') {
+        newAuthor();
+        callback();
+      }
+
+      //  }, 1000/(args.options.authors || 1));
+    }, 200 / (users.length || 1));
+    // All authors connect within 1 second but send messages on
+    // slightly different intervals
+    // This need slightly different logic
+  }, (err) => {
+  });
+} else {
+  if (!endTime) {
+    console.log('Creating load until the pad server stops responding in a timely fashion');
+  }
+  if (endTime) {
+    const testD = Math.round((endTime - Date.now()) / 1000);
+    console.log(`Creating load for ${testD} seconds`);
+  }
+  loadUntilFailFn();
+}
--- multi.js
+++ multi.js
@@ -2,20 +2,20 @@
 'use strict';
 
 // tests 10 pads (unless specified by "node multi.js X[int]" for 30 seconds
-const child_process = require('child_process');
+const childProcess = require('child_process');
 
 const maxPads = process.argv[2] || 10; // 10 padIds
 let padCount = 0;
 const messageCount = 0;
 
 while (padCount < maxPads) {
-  const child = child_process.fork(
+  const child = childProcess.fork(
       '/lib/node_modules/etherpad-load-test/app.js', ['-a', 3, '-d', 30]);
   padCount++;
   // below still not working...
   child.on('error', () => {
     console.log('total pads made');
     console.log('total messages', messageCount);
-    process.exit(1); /* eslint-disable-line no-process-exit */
+    process.exit(1); /* eslint-disable-line n/no-process-exit */
   });
 }
